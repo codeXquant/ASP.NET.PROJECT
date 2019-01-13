d215e456f2cf02b3f2742e00587351b1196634cd
dachangzhu <zdcxisi@163.com>
2019-01-13 14:44:03 +0800

    第一次提交ASP.NET.CORE代码，学习到如何读取配置文件数据

---
+++ CMSCORE/CMS.CORE.WEB/CMS.CORE.WEB/Controllers/ContentController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CMS.CORE.WEB.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace CMS.CORE.WEB.Controllers
+{
+    public class ContentController : Controller
+    {
+        private Content _content = null;
+        public ContentController(IOptions<Content> option)
+        {
+            _content = option.Value;
+        }
+
+        // GET: Content
+        public ActionResult Index()
+        {
+            return View(new ContentViewModel()
+            {
+                Contents = new List<Content>()
+                {
+                    _content,_content
+                }
+            });
+            //var contents = new List<Content>();
+            //for (int i = 0; i < 11; i++)
+            //{
+            //    contents.Add(new Content
+            //    {
+            //        Id = i,
+            //        title = $"{i}的标题",
+            //        content = $"{i}的内容",
+            //        status = 1,
+            //        add_time = DateTime.Now.AddDays(-i)
+            //    });
+            //}
+            //return View(new ContentViewModel { Contents = contents });
+        }
+
+        // GET: Content/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Content/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Content/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Content/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Content/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Content/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Content/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CMSCORE/CMS.CORE.WEB/CMS.CORE.WEB/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace CMS.CORE.WEB
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
+            // 启动一个一直运行监听http请求的任务
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)// 加载默认配置文件appsetting.json文件
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("Content.json", optional: false, reloadOnChange: true).AddEnvironmentVariables();
+            })    
+            .UseStartup<Startup>();
+    }
+}
